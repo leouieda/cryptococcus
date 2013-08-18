0f836fbce826a6729a6286cda6a302760da6fb22
Leonardo Uieda <leouieda@gmail.com>
2013-08-18 19:30:28 -0300

    Implemented opening images and zoom

--- fungui/app.py
+++ fungui/app.py
@@ -1,33 +1,40 @@
 """
 fungui is a software to help measuring the shell of a fungi.
 """
-from PyQt4 import QtGui
+from __future__ import division
+from PyQt4 import QtGui, QtCore
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 
 
 class MainWindow(QtGui.QMainWindow):
 
-    width = 1020
-    height = 480
+    width = 1000
+    height = 600
+    appname = 'FunGUI'
 
     def __init__(self):
         super(MainWindow, self).__init__()
         self.populate()
+        self.zoom_level = 1
 
     def populate(self):
         "Initializes the GUI and populates with widgets."
-        self.wdg = Widget()
-        self.setCentralWidget(self.wdg)
-        self.createActions()
-        self.createMenus()
+        self.image_widget = QtGui.QLabel()
+        self.image_widget.setSizePolicy(QtGui.QSizePolicy.Ignored,
+            QtGui.QSizePolicy.Ignored)
+        self.image_widget.setScaledContents(True)
+        self.image_pane = QtGui.QScrollArea()
+        self.image_pane.setWidget(self.image_widget)
+        self.setCentralWidget(self.image_pane)
+        self.create_actions()
+        self.create_menus()
         self.resize(self.width, self.height)
         self.center()
-        self.setWindowTitle('Fungui')
-        self.show()
-        self.wdg.show()
+        self.setWindowTitle(self.appname)
 
     def center(self):
+        "Center the window on the screen"
         qr = self.frameGeometry()
         cp = QtGui.QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
@@ -37,102 +44,70 @@ class MainWindow(QtGui.QMainWindow):
         QtGui.QMessageBox.about(self, self.tr("About fungui"),
             self.tr("fungui..."))
 
-    def createActions(self):
-        self.exitAct = QtGui.QAction(self.tr("E&xit"), self)
-        self.exitAct.setShortcut(self.tr("Ctrl+Q"))
-        self.exitAct.setStatusTip(self.tr("Exit the application"))
-        self.exitAct.triggered.connect(self.close)
+    def create_actions(self):
+        "Create actions and connect them to the proper functions/methods"
+        self.exit_act = QtGui.QAction(self.tr("E&xit"), self,
+            shortcut="Ctrl+Q", triggered=self.close)
+        self.about_act = QtGui.QAction(self.tr("&About"), self,
+            triggered=self.about)
+        self.open_img_act = QtGui.QAction(self.tr("&Open"), self,
+            shortcut="Ctrl+O", triggered=self.open_img)
+        self.zoom_in_act = QtGui.QAction(self.tr("Zoom &In"), self,
+            shortcut="+", triggered=self.zoom_in)
+        self.zoom_out_act = QtGui.QAction(self.tr("Zoom &Out"), self,
+            shortcut="-", triggered=self.zoom_out)
 
-        self.aboutAct = QtGui.QAction(self.tr("&About"), self)
-        self.aboutAct.setStatusTip(self.tr("Show the application's About box"))
-        self.aboutAct.triggered.connect(self.about)
+    def create_menus(self):
+        "Connect menu items with actions"
+        self.file_menu = self.menuBar().addMenu(self.tr("&File"))
+        self.file_menu.addAction(self.open_img_act)
+        self.file_menu.addAction(self.exit_act)
+        self.image_menu = self.menuBar().addMenu(self.tr("&Image"))
+        self.image_menu.addAction(self.zoom_in_act)
+        self.image_menu.addAction(self.zoom_out_act)
+        self.help_menu = self.menuBar().addMenu(self.tr("&Help"))
+        self.help_menu.addAction(self.about_act)
 
-        self.aboutQtAct = QtGui.QAction(self.tr("About &Qt"), self)
-        self.aboutQtAct.setStatusTip(self.tr("Show the Qt library's About box"))
-        self.aboutQtAct.triggered.connect(QtGui.qApp.aboutQt)
+    def open_img(self):
+        "Starts a file browser to select an image to open."
+        fname = QtGui.QFileDialog.getOpenFileName(self, "Open File",
+                QtCore.QDir.currentPath())
+        if fname:
+            image = QtGui.QImage(fname)
+            if image.isNull():
+                QtGui.QMessageBox.information(self, "Oops",
+                        "Cannot load %s." % fname)
+                return
+            self.image_widget.setPixmap(QtGui.QPixmap.fromImage(image))
+            # Make the image fit one of the dimensions of the window
+            size = self.image_widget.pixmap().size()
+            ih, iw = size.height(), size.width()
+            h, w = self.size().height(), self.size().width()
+            factor = min(h/ih, w/iw)
+            self.scale_img(0.99*factor)
+            self.zoom_level = 1.0
+            # Makes the image be the exact size of the window
+            #self.image_pane.setWidgetResizable(True)
+            self.setWindowTitle(self.appname + ' - ' + fname)
 
-    def createMenus(self):
-        self.fileMenu = self.menuBar().addMenu(self.tr("&File"))
-        self.fileMenu.addAction(self.exitAct)
+    def zoom_in(self):
+        self.zoom_level *= 1.25
+        self.scale_img(self.zoom_level)
+        self.zoom_in_act.setEnabled(self.zoom_level < 3.0)
 
-        self.helpMenu = self.menuBar().addMenu(self.tr("&Help"))
-        self.helpMenu.addAction(self.aboutAct)
-        self.helpMenu.addAction(self.aboutQtAct)
+    def zoom_out(self):
+        self.zoom_level *= 0.8
+        self.scale_img(self.zoom_level)
+        self.zoom_out_act.setEnabled(self.zoom_level > 0.333)
 
-class Widget(QtGui.QWidget):
+    def scale_img(self, factor):
+        "Scaled the image by a factor"
+        self.image_widget.resize(
+                factor*self.image_widget.pixmap().size())
+        self.adjust_scrollbar(self.image_pane.horizontalScrollBar(), factor)
+        self.adjust_scrollbar(self.image_pane.verticalScrollBar(), factor)
 
-    def __init__(self):
-        super(Widget, self).__init__()
-        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
-        self.create_frame()
+    def adjust_scrollbar(self, scrollbar, factor):
+        scrollbar.setValue(int(
+            factor*scrollbar.value() + ((factor - 1)*scrollbar.pageStep()/2)))
 
-    def create_frame(self):
-        """The frame"""
-        self.main_frame = QtGui.QWidget()
-
-        # Create the mpl Figure and FigCanvas objects.
-        # 5x4 inches, 100 dots-per-inch
-        #
-        self.dpi = 100
-        self.fig = Figure((5.0, 4.0), dpi = self.dpi)
-        self.canvas = FigureCanvas(self.fig)
-        self.canvas.setParent(self.main_frame)
-
-        # Since we have only one plot, we can use add_axes
-        # instead of add_subplot, but then the subplot
-        # configuration tool in the navigation toolbar wouldn't
-        # work.
-        #
-        self.axes = self.fig.add_subplot(111)
-
-        self.on_draw()
-
-
-        # button to open image through file dialog
-        self.open_button = QtGui.QPushButton('Open', self)
-        self.open_button.clicked.connect(self.file_dialog)
-        self.open_button.setToolTip('Open image')
-        #self.obs_button.resize(self.open_button.sizeHint())
-        self.open_button.setMaximumWidth(60)
-
-
-        #
-        # Layout with box sizers
-        #
-        # define grid
-        grid = QtGui.QGridLayout()
-        #grid.setSpacing(10)
-
-        #set matplotlib canvas
-        grid.addWidget(self.canvas, 0, 1, 6, 1)
-        #open image button
-        grid.addWidget(self.open_button, 0, 0)
-
-        self.setLayout(grid)
-
-        self.show()
-
-
-    # Draw canvas
-    def on_draw(self):
-        """ draws the figure"""
-        # clear the axes and redraw the plot anew
-        #
-        self.axes.clear()
-
-        self.axes.plot(range(10), [i**2 for i in range(10)])
-
-        self.axes.set_title('Image name', fontsize= 'small')
-
-        self.canvas.draw()
-
-
-    # Support Modules
-    def file_dialog(self):
-        """Open a file dialog"""
-        #self.fileDialog = QtGui.QFileDialog(self)
-        #self.fileDialog.show()
-        fname = str(QtGui.QFileDialog.getOpenFileName(self,"Open File"))
-        if fname != '':
-            # here we should set a variable to passname
-            pass
