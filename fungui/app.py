8d2fc8eff7a4748938c7283d2436b3ab1b3e8219
Leonardo Uieda <leouieda@gmail.com>
2013-08-19 18:03:36 -0300

    Added color to selection rectangle (ugly code)
    w @gabraganca

--- fungui/app.py
+++ fungui/app.py
@@ -13,11 +13,12 @@ class App(object):
 
     def __init__(self):
         super(App, self).__init__()
-        self.imgsize = None
+        self.img_size = None
         self.image_path = QtCore.QDir.currentPath()
         self.image_window = ImageWindow()
         self.image_window.open_img = self.open_img
         self.image_window.select = self.select
+        self.image_window.show_selection = self.show_selection
         self.image_window.connect()
         self.image_window.setWindowTitle(self.appname)
         self.image_window.center()
@@ -30,31 +31,44 @@ class App(object):
         "Starts a file browser to select an image to open."
         fname = QtGui.QFileDialog.getOpenFileName(self.image_window,
             "Open File", self.image_path)
-        self.image_path = os.path.dirname(str(fname))
         if fname:
             image = QtGui.QImage(fname)
             if image.isNull():
                 QtGui.QMessageBox.information(self.image_window, "Oops",
                         "Cannot load %s." % fname)
                 return
+            self.image_path = os.path.dirname(str(fname))
             self.image_window.image_widget.setPixmap(
                 QtGui.QPixmap.fromImage(image))
-            self.imgsize = self.image_window.image_widget.pixmap().size()
-            w = self.image_window.size().width()
-            h = w*self.imgsize.height()/self.imgsize.width()
-            self.image_window.resize(w, h)
-            self.image_window.image_widget.resize(w, h)
+            self.resize_with_ratio(self.image_window, image)
             self.image_window.setWindowTitle(
                 ' - '.join([self.appname, str(fname)]))
             self.selection_window.image_widget.clear()
             self.selection_window.initial_size()
 
+    def resize_with_ratio(self, window, image):
+        w = window.size().width()
+        h = w*image.height()/image.width()
+        window.resize(w, h)
+        window.image_widget.resize(w, h)
+
     def select(self):
+        self.image_window.select_act.setEnabled(False)
+
+    def show_selection(self, rectangle):
         image = self.image_window.image_widget.pixmap()
-        i, j, w, h = 0, 0, 200, 500
+        i, j, w, h = rectangle.getRect()
+        # Need to scale the selection to image coordinates. The rect is in
+        # window coordinates
+        wfactor = image.width()/self.image_window.size().width()
+        hfactor = image.height()/self.image_window.size().height()
+        i *= wfactor
+        w *= wfactor
+        j *= hfactor
+        h *= hfactor
         selection = image.copy(i, j, w, h)
         self.selection_window.image_widget.setPixmap(selection)
-        self.selection_window.resize(w, h)
+        self.resize_with_ratio(self.selection_window, selection)
         self.selection_window.setWindowTitle(
                 ' - '.join([self.appname, 'Selection']))
         self.selection_window.show()
--- fungui/gui.py
+++ fungui/gui.py
@@ -5,6 +5,30 @@ from __future__ import division
 from PyQt4 import QtGui, QtCore
 
 
+class SelectionRectangle(QtGui.QRubberBand):
+    """
+    The selection rectagle.
+
+    Modified from:
+    http://www.qtcentre.org/threads/26140-Change-the-color-of-QRubberBand
+    """
+
+    color = QtGui.QColor(200, 0, 0)
+
+    def __init__(self, parent):
+        super(SelectionRectangle, self).__init__(QtGui.QRubberBand.Rectangle,
+            parent)
+        palette = QtGui.QPalette()
+        palette.setBrush(QtGui.QPalette.WindowText,
+            QtGui.QBrush(self.color))
+        self.setPalette(palette)
+
+    def paintEvent(self, e):
+        painter = QtGui.QStylePainter(self)
+        option = QtGui.QStyleOptionFocusRect()
+        option.initFrom(self)
+        painter.drawControl(QtGui.QStyle.CE_FocusFrame, option)
+
 class ImageWindow(QtGui.QMainWindow):
 
     starting_width = 600
@@ -15,7 +39,9 @@ class ImageWindow(QtGui.QMainWindow):
         self.image_widget.setSizePolicy(QtGui.QSizePolicy.Ignored,
             QtGui.QSizePolicy.Ignored)
         self.image_widget.setScaledContents(True)
+        self.image_widget.setMouseTracking(True)
         self.setCentralWidget(self.image_widget)
+        self.selection_rect = SelectionRectangle(self.image_widget)
 
     def initial_size(self):
         self.resize(self.starting_width, self.starting_width)
@@ -44,7 +70,7 @@ class ImageWindow(QtGui.QMainWindow):
         self.open_img_act = QtGui.QAction(self.tr("&Open"), self,
             shortcut="Ctrl+O", triggered=self.open_img)
         self.select_act = QtGui.QAction(self.tr("&Select"), self,
-            shortcut="Ctrl+L", triggered=self.select)
+            shortcut="Ctrl+L", enabled=True, triggered=self.select)
 
     def create_menus(self):
         "Connect menu items with actions"
@@ -56,9 +82,30 @@ class ImageWindow(QtGui.QMainWindow):
         self.help_menu = self.menuBar().addMenu(self.tr("&Help"))
         self.help_menu.addAction(self.about_act)
 
+    def mousePressEvent(self, event):
+        if not self.select_act.isEnabled():
+            self.select_origin = event.pos()
+            self.selection_rect.setGeometry(
+                QtCore.QRect(self.select_origin, QtCore.QSize()))
+            self.selection_rect.show()
+        QtGui.QWidget.mousePressEvent(self, event)
+
+    def mouseMoveEvent(self, event):
+        if not self.select_act.isEnabled():
+            self.selection_rect.setGeometry(
+                QtCore.QRect(self.select_origin, event.pos()).normalized())
+        QtGui.QWidget.mouseMoveEvent(self, event)
+
+    def mouseReleaseEvent(self, event):
+        if not self.select_act.isEnabled():
+            #self.selection_rect.hide()
+            self.show_selection(self.selection_rect.geometry())
+            self.select_act.setEnabled(True)
+        QtGui.QWidget.mouseReleaseEvent(self, event)
+
 class SelectionWindow(ImageWindow):
 
-    starting_width = 300
+    starting_width = 500
 
     def __init__(self):
         super(SelectionWindow, self).__init__()
@@ -72,3 +119,12 @@ class SelectionWindow(ImageWindow):
         "Connect menu items with actions"
         self.selection_menu = self.menuBar().addMenu(self.tr("Selection"))
         self.selection_menu.addAction(self.close_act)
+
+    def mousePressEvent(self, event):
+        QtGui.QWidget.mousePressEvent(self, event)
+
+    def mouseMoveEvent(self, event):
+        QtGui.QWidget.mouseMoveEvent(self, event)
+
+    def mouseReleaseEvent(self, event):
+        QtGui.QWidget.mouseReleaseEvent(self, event)
